ecdb6543b53604e57037e97fc908b2b73c566b4a
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2024-11-20 12:03:46 +0200

    chore: update envio endpoint

--- examples/wagmi/src/constants/indexers.ts
+++ examples/wagmi/src/constants/indexers.ts
@@ -1,2 +1,2 @@
 export const INDEXER_THE_GRAPH = "https://api.studio.thegraph.com/query/57079/sablier-v2-sepolia/version/latest";
-export const INDEXER_ENVIO = "https://indexer.bigdevenergy.link/3b4ea6b/v1/graphql";
+export const INDEXER_ENVIO = "https://indexer.hyperindex.xyz/3b4ea6b/v1/graphql";
