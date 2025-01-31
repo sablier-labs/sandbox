dc34e8ae07064cf5fea51f741a491c35c802bb23
Mircea Gavriliu <gavriliumircea@yahoo.ro>
2025-01-31 08:56:44 +0200

    chore: update indexers

--- examples/wagmi/src/constants/indexers.ts
+++ examples/wagmi/src/constants/indexers.ts
@@ -1,2 +1,2 @@
-export const INDEXER_THE_GRAPH = "https://api.studio.thegraph.com/query/57079/sablier-v2-sepolia/version/latest";
-export const INDEXER_ENVIO = "https://indexer.hyperindex.xyz/3b4ea6b/v1/graphql";
+export const INDEXER_THE_GRAPH = "https://api.studio.thegraph.com/query/57079/sablier-lockup-sepolia/version/latest";
+export const INDEXER_ENVIO = "https://indexer.hyperindex.xyz/53b7e25/v1/graphql";
